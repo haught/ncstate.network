931f85ecef79a40d7dedc6af9a0c4b9dbbebe778
Matt Haught <dmhaught@ncsu.edu>
2021-01-13 12:03:26 -0500

    fix edgeswitch_module in tests

--- tests/unit/plugins/modules/edgeswitch/test_edgeswitch_command.py
+++ tests/unit/plugins/modules/edgeswitch/test_edgeswitch_command.py
@@ -24,7 +24,7 @@ import json
 from ansible_collections.community.network.tests.unit.compat.mock import patch
 from ansible_collections.community.network.plugins.modules.network.edgeswitch import edgeswitch_command
 from ansible_collections.community.network.tests.unit.plugins.modules.utils import set_module_args
-from .edgeswitch_module import TestEdgeswitchModule, load_fixture
+from ansible_collections.community.network.tests.unit.plugins.modules.network.edgeswitch.edgeswitch_module import TestEdgeswitchModule, load_fixture
 
 
 class TestEdgeswitchCommandModule(TestEdgeswitchModule):
